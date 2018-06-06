6ebf6e14cecc336e1773eff9272adcbac31b3862
Ivan7315 <2929422190@qq.com>
2018-06-06 16:21:33 +0800

    upgrade async&await

--- www/app.py
+++ www/app.py
@@ -7,11 +7,12 @@ from aiohttp import web
 def index(request):
     return web.Response(body='<h1>Awesome</h1>')
 
-@asyncio.coroutine
-def init(loop):
+#@asyncio.coroutine
+async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1',8000)
+#     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1',8000)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1',8000)
     logging.info('server started at http://127.0.0.1:8000')
     return srv
     
